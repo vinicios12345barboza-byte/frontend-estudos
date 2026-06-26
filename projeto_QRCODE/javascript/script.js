b14558fb6f6ce05c7bac9ff6c5577e7136bf96fa
Vinicios Barboza <vinicios12345barboza@gmail.com>
2026-06-26 16:25:59 -0300

    feat: projeto gerador de QR CODE.

---
+++ projeto_QRCODE/javascript/script.js
@@ -0,0 +1,43 @@
+const container = document.querySelector(".container")
+const qrCodeBtn = document.querySelector("#qr-form button")
+const qrCodeInput = document.querySelector("#qr-form input")
+
+const qrCodeImg = document.querySelector("#qr-code img")
+
+// eventos
+
+const generatorQrCode = () =>{
+    const qrCodeInputValue = qrCodeInput.value
+
+    if(!qrCodeInputValue) return;
+
+    qrCodeBtn.innerText = "Gerando código.."
+
+    qrCodeImg.src = ` https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`
+
+    qrCodeImg.addEventListener("load", () =>{
+        container.classList.add("active")
+        qrCodeBtn.innerText = "Código criado."
+    })
+
+}
+
+qrCodeBtn.addEventListener("click", (e) =>{
+    e.preventDefault()
+    generatorQrCode()
+})
+
+qrCodeInput.addEventListener("keydown", (e) =>{
+   if(e.key === "Enter"){
+    generatorQrCode()
+   }
+})
+
+// limpar area qr code
+qrCodeInput.addEventListener("keyup", () =>{
+
+    if(!qrCodeInput.value){
+        container.classList.remove("active")
+        qrCodeBtn.innerText = "Gerar QR CODE"
+    }
+})
